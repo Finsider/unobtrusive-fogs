5d5e2690aefd7acbf2ee0f57ece39784ff8a23c3
ajayanjay <muhammad.azzar.tif424@polban.ac.id>
2026-03-16 22:43:21 +0700

    1.21.1 port

--- src/main/java/fin/unobtrusivefog/mixin/BackgroundRendererMixin.java
+++ src/main/java/fin/unobtrusivefog/mixin/BackgroundRendererMixin.java
@@ -1,22 +1,21 @@
 package fin.unobtrusivefog.mixin;
 
+import com.llamalad7.mixinextras.sugar.Local;
 import fin.unobtrusivefog.FogRenderType;
 import fin.unobtrusivefog.Main;
 import fin.unobtrusivefog.Settings;
 import net.minecraft.block.enums.CameraSubmersionType;
 import net.minecraft.client.render.BackgroundRenderer;
 import net.minecraft.client.render.Camera;
-import net.minecraft.client.render.Fog;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.effect.StatusEffects;
 import net.minecraft.world.World;
-import org.joml.Vector4f;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(BackgroundRenderer.class)
 public class BackgroundRendererMixin {
@@ -27,16 +26,14 @@ public class BackgroundRendererMixin {
     @Inject(
             method = "applyFog",
             at = @At(
-                    value = "RETURN"
-            ),
-            cancellable = true
+                    value = "INVOKE",
+                    target = "Lcom/mojang/blaze3d/systems/RenderSystem;setShaderFogStart(F)V"
+            )
     )
-    private static void modifyFog(Camera camera, BackgroundRenderer.FogType fogType, Vector4f color, float viewDistance, boolean thickenFog, float tickProgress, CallbackInfoReturnable<Fog> cir) {
+    private static void modifyFog(Camera camera, BackgroundRenderer.FogType fogType, float viewDistance, boolean thickFog, float tickDelta, CallbackInfo ci, @Local CameraSubmersionType cameraSubmersionType, @Local Entity entity, @Local BackgroundRenderer.FogData fogData) {
 
         // peak code writing here
         if (!SETTINGS.applyToAll) {
-            CameraSubmersionType cameraSubmersionType = camera.getSubmersionType();
-            Entity entity = camera.getFocusedEntity();
             World world = entity.getWorld();
 
             if (
@@ -55,16 +52,12 @@ public class BackgroundRendererMixin {
             ) return;
         }
 
-        Fog fog = cir.getReturnValue();
         float fogEnd = viewDistance;
-        float end = fog.end();
 
         if (SETTINGS.renderDistanceFogType == FogRenderType.DISABLED) {
-            end = Integer.MAX_VALUE;
-        } else if (SETTINGS.renderDistanceFogType == FogRenderType.UNOBTRUSIVE && end < fogEnd) {
-            end = fogEnd;
+            fogData.fogEnd = Integer.MAX_VALUE;
+        } else if (SETTINGS.renderDistanceFogType == FogRenderType.UNOBTRUSIVE && fogData.fogEnd < fogEnd) {
+            fogData.fogEnd = fogEnd;
         }
-
-        cir.setReturnValue(new Fog(fog.start(), end, fog.shape(), fog.red(), fog.green(), fog.blue(), fog.alpha()));
     }
 }
